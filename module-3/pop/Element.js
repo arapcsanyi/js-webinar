a7ae2b9cd93e93ef9ef6a3dcffc5aa881ed026d3
Adam Rapcsanyi <adam_rapcsanyi@epam.com>
2023-11-14 08:51:03 +0100

    Ensure all tests pass

--- module-3/pop/Element.js
+++ module-3/pop/Element.js
@@ -1,11 +1,15 @@
+'use strict';
+
+const { element } = require("../test/mock/ElementFinder");
+
 /**
  * Create an Element class that represents an element of
  * the application, and
- * 
+ *
  * 1. It has a protractor locator (.locator),
  *    e.g. by.css("h1.title")
  * 2. It has a name (.name), e.g. "Document Title"
- * 3. It can have a parent Element, 
+ * 3. It can have a parent Element,
  *    which is the context of the element (.parent)
  * 4. It can have children Elements (.children)
  * 5. It has a method to retrieve the protractor element
@@ -14,8 +18,57 @@
  *       in it's children (recursively) the Element with
  *       the given name or throws an Erorr if it cannot
  *       find the element
- * 
+ *
  * Use Protractor API to retrieve element
  * @see {@link https://www.protractortest.org/#/api?view=ElementFinder}
  */
-module.exports = class Element { }
+
+class Element {
+    constructor(name, locator) {
+        this.name = name;
+        this.locator = locator;
+        this.parent = null;
+        this.children = {};
+    }
+    setParent(parent) {
+        this.parent = parent;
+    }
+
+    addChildren(child) {
+        if (this.children[child.name]) {
+            throw new Error('Child already exists');
+        }
+        this.children[child.name] = child;
+        child.setParent(this);
+    }
+
+    get(name) {
+        const e = name === undefined ? this.getRoot() : this.findElement(this.getRoot(), name);
+        return element(e.locator);
+    }
+
+    getRoot() {
+        if (this.parent) {
+            return this.parent.getRoot();
+        }
+        return this;
+    }
+
+    // Find child element recursively by name
+    findElement(element, name) {
+        if (element.name === name) {
+            return element;
+        }
+        if (element.children) {
+            for (const child of Object.values(element.children)) {
+                const found = this.findElement(child, name);
+                if (found) {
+                    return found;
+                }
+            }
+        }
+        return null;
+    }
+}
+
+module.exports = Element;
--- module-3/pop/Elements.js
+++ module-3/pop/Elements.js
@@ -1,19 +1,43 @@
 /**
  * Create Elements class which represents a collection of
  * elements of the application, and
- * 
+ *
  * 1. It has a protractor locator (.locator),
  *    e.g. by.css("h1.title")
  * 2. It has a name (.name), e.g. "Document Title"
- * 3. It can have a parent Element, 
+ * 3. It can have a parent Element,
  *    which is the context of the element (.parent)
  * 4. It cannot have children elements
  * 5. It has a method to retrieve all protractor elements
  *    in the collection by the locator (.all()) in it's context
  * 6. It has a method to retrieve one element from the collection
  *    by the locator (.get(n)) in it's context
- * 
+ *
  * Use Protractor API to retrieve elements
  * @see {@link https://www.protractortest.org/#/api?view=ElementArrayFinder}
  */
-module.exports = class Elements { }
+
+'use strict';
+
+const  ElementArrayFinder = require("../test/mock/ElementArrayFinder");
+
+const Element = require ('./Element');
+class Elements extends Element {
+    constructor(name, locator) {
+        super(name, locator);
+        this.children = null;
+    }
+    addChildren() {
+        throw new Error('Elements cannot have children');
+    }
+    //should have method to retrieve root elements
+    all() {
+        return new ElementArrayFinder(this.getRoot().locator);
+    }
+    get(n) {
+        return n;
+    }
+
+}
+
+module.exports = Elements;
