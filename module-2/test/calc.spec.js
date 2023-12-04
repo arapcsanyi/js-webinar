80d8386b75e9ff3a66a73bf766fa99af31512123
Adam Rapcsanyi <adam_rapcsanyi@epam.com>
2023-12-04 10:12:51 +0100

    Refactoring and adding new tests

--- module-2/test/calc.spec.js
+++ module-2/test/calc.spec.js
@@ -23,44 +23,80 @@ describe.only('calc', () => {
         expect(calc(3).v).to.equal(3);
     });
 
-    it('should correctly add two numbers', () => {
-        expect(calc(3).add(5).v).to.equal(8);
+    describe("Addition", () => {
+        it('should exist', () => {
+            expect(calc(3).add).to.exist;
+        });
+        it('should correctly add two numbers', () => {
+            expect(calc(3).add(5).v).to.equal(8);
+        });
     });
 
-    it('should correctly subtract one number from another', () => {
-        expect(calc(3).minus(2).v).to.equal(1);
+    describe("Subtraction", () => {
+
+        it('should exist', () => {
+            expect(calc(3).minus).to.exist;
+        });
+        it('should correctly subtract one number from another', () => {
+            expect(calc(3).minus(2).v).to.equal(1);
+        });
     });
 
-    it('should correctly calculate the square root of a number', () => {
-        expect(calc(4).sqrt().v).to.equal(2);
+    describe("Square Root", () => {
+
+        it('should exist', () => {
+            expect(calc(4).sqrt).to.exist;
+        });
+        it('should correctly calculate the square root of a number', () => {
+            expect(calc(4).sqrt().v).to.equal(2);
+        });
+        it('should throw an error when taking the square root of a negative number', () => {
+            expect(() => calc(-3).sqrt()).to.throw('Square root of a negative value cannot be determined!');
+        });
     });
 
-    it('should correctly multiply two numbers', () => {
-        expect(calc(3).times(10).v).to.equal(30);
+    describe("Multiplication", () => {
+
+        it('should exist', () => {
+            expect(calc(3).times).to.exist;
+        });
+        it('should correctly multiply two numbers', () => {
+            expect(calc(3).times(10).v).to.equal(30);
+        });
     });
 
-    it('should correctly divide one number by another', () => {
-        expect(calc(10).divide(2).v).to.equal(5);
+    describe("Division", () => {
+
+        it('should exist', () => {
+            expect(calc(10).divide).to.exist;
+        });
+        it('should correctly divide one number by another', () => {
+            expect(calc(10).divide(2).v).to.equal(5);
+        });
+        it('should throw an error when dividing by zero', () => {
+            expect(() => calc(5).divide(0)).to.throw('Division by 0 is not possible!');
+        });
     });
 
-    it('should correctly calculate the modulo of one number by another', () => {
-        expect(calc(10).modulo(5).v).to.equal(0);
+    describe("Modulo", () => {
+
+        it('should exist', () => {
+            expect(calc(10).modulo).to.exist;
+        });
+        it('should correctly calculate the modulo of one number by another', () => {
+            expect(calc(10).modulo(5).v).to.equal(0);
+        });
     });
 
-    it('should throw an error when dividing by zero', () => {
-        expect(() => calc(5).divide(0)).to.throw('Division by 0 is not possible!');
-    });
+    describe("Chaining", () => {
 
-    it('should throw an error when taking the square root of a negative number', () => {
-        expect(() => calc(-3).sqrt()).to.throw('Square root of a negative value cannot be determined!');
-    });
-
-    it('should correctly chain multiple operations', () => {
-        expect(
-            calc(3)
-                .add(4)
-                .minus(3)
-                .times(6).v
-        ).to.equal(24);
+        it('should correctly chain multiple operations', () => {
+            expect(
+                calc(3)
+                    .add(4)
+                    .minus(3)
+                    .times(6).v
+            ).to.equal(24);
+        });
     });
 });
